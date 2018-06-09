4a5af354e93f5698b8ea3c88600190a860286fdd
Anuj Kumar <cdac.anuj@gmail.com>
2018-06-09 15:53:03 +0530

    File downloading

---
+++ src/test/java/auto/testcases/TestFileDownloader.java
@@ -0,0 +1,27 @@
+package auto.testcases;
+/**
+ * Created by: Anuj Kumar Email: cdac.anuj@gmail.com Date: 21-May-18
+ */
+
+import org.testng.annotations.Test;
+import auto.pages.FileDownloaderPage;
+import auto.pages.WelcomePage;
+import auto.utility.Init;
+
+public class TestFileDownloader extends Init {
+
+  @Test
+  public void testFrame() throws InterruptedException {
+
+    WelcomePage welcomePage = new WelcomePage(driver);
+    welcomePage.verifyWelcomePageTitle().verifyWelcomePageHeader().clickOnLink("File Download");
+
+    FileDownloaderPage downloaderPage = new FileDownloaderPage(driver);
+    downloaderPage.verifyFileDownloaderHeader();
+    downloaderPage.verifyFileDownload();
+
+    Thread.sleep(5000);
+  }
+
+
+}
