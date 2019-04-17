c9cac14e8c0fbe8fd90280767a60697070645a1e
Anuj Kumar <cdac.anuj@gmail.com>
2019-04-17 19:29:30 +0530

    Added logger.

--- src/test/java/auto/pages/AlertPage.java
+++ src/test/java/auto/pages/AlertPage.java
@@ -1,5 +1,14 @@
 package auto.pages;
 
+import static org.testng.Assert.assertEquals;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
 /**
  * Created by: Anuj Kumar
  * Email: cdac.anuj@gmail.com
@@ -7,70 +16,65 @@ package auto.pages;
  */
 
 import auto.utility.Services;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-
-import static org.testng.Assert.assertEquals;
 
 public class AlertPage extends Services {
+	
+	static Logger logger = Logger.getLogger(AlertPage.class.getName());
 
-    private final static String HEADING = "JavaScript Alerts";
-    private static final String SUCCESS_MSG = "You successfuly clicked an alert";
-    private static final String CANCEL_MSG = "You clicked: Cancel";
-    private static final String HELLO_MSG = "You entered: Hello";
-    private String xpathHeading = "//h3";
-    private String xpathButtons = "//button[text()='**dummy**']";
-    private String xpathResult = "//p[@id='result']";
+	private final static String HEADING = "JavaScript Alerts";
+	private static final String SUCCESS_MSG = "You successfuly clicked an alert";
+	private static final String CANCEL_MSG = "You clicked: Cancel";
+	private static final String HELLO_MSG = "You entered: Hello";
+	private String xpathHeading = "//h3";
+	private String xpathButtons = "//button[text()='**dummy**']";
+	private String xpathResult = "//p[@id='result']";
 
-    public AlertPage(WebDriver driver) {
-        super(driver);
-    }
+	public AlertPage(WebDriver driver) {
+		super(driver);
+	}
 
-    public void verifyAlertPageHeader() {
-        waitForElement(xpathHeading);
-        WebElement headerEle = driver.findElement(By.xpath(xpathHeading));
-        String actualHeading = headerEle.getText();
-        assertEquals(actualHeading, HEADING,
-                "Actual heading '" + actualHeading + "' should be same as expected '" + HEADING + "'.");
-    }
+	public void verifyAlertPageHeader() {
+		waitForElement(xpathHeading);
+		WebElement headerEle = driver.findElement(By.xpath(xpathHeading));
+		String actualHeading = headerEle.getText();
+		logger.info("# Actual Heading: " + actualHeading);
+		assertEquals(actualHeading, HEADING,
+				"Actual heading '" + actualHeading + "' should be same as expected '" + HEADING + "'.");
+	}
 
-    public void clickOnButton(String btn) {
+	public void clickOnButton(String btn) {
 
-        String xpath = xpathButtons.replace("**dummy**", btn);
-        getWebElement(xpath).click();
+		String xpath = xpathButtons.replace("**dummy**", btn);
+		getWebElement(xpath).click();
 
-    }
+	}
 
-    public void accept() {
-        Alert alert = driver.switchTo().alert();
-        System.out.println(alert.getText());
-        alert.accept();
+	public void accept() {
+		Alert alert = driver.switchTo().alert();
+		logger.info("# Text on alert: " + alert.getText());
+		alert.accept();
 
-        String actual = getWebElement(xpathResult).getText();
-        Assert.assertEquals(actual, SUCCESS_MSG);
+		String actual = getWebElement(xpathResult).getText();
+		Assert.assertEquals(actual, SUCCESS_MSG);
 
+	}
 
-    }
+	public void cancel() {
+		Alert alert = driver.switchTo().alert();
+		alert.dismiss();
+		String actual = getWebElement(xpathResult).getText();
+		Assert.assertEquals(actual, CANCEL_MSG);
 
-    public void cancel() {
-        Alert alert = driver.switchTo().alert();
-        alert.dismiss();
-        String actual = getWebElement(xpathResult).getText();
-        Assert.assertEquals(actual, CANCEL_MSG);
+	}
 
-    }
+	public void typeAndAccept() {
+		Alert alert = driver.switchTo().alert();
+		alert.sendKeys("Hello");
+		alert.accept();
 
-    public void typeAndAccept() {
-        Alert alert = driver.switchTo().alert();
-        alert.sendKeys("Hello");
-        alert.accept();
+		String actual = getWebElement(xpathResult).getText();
+		Assert.assertEquals(actual, HELLO_MSG);
 
-        String actual = getWebElement(xpathResult).getText();
-        Assert.assertEquals(actual, HELLO_MSG);
-
-    }
+	}
 
 }
--- src/test/java/auto/utility/Services.java
+++ src/test/java/auto/utility/Services.java
@@ -1,5 +1,7 @@
 package auto.utility;
 
+import org.apache.log4j.Logger;
+
 /**
  * Created by: Anuj Kumar
  * Email: cdac.anuj@gmail.com
@@ -15,94 +17,74 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-
 public class Services {
+	static Logger logger = Logger.getLogger(Services.class.getName());
+	protected WebDriver driver;
 
-    protected WebDriver driver;
+	public Services(WebDriver driver) {
+		this.driver = driver;
+	}
 
-    public Services(WebDriver driver) {
-        this.driver = driver;
-    }
+	public void waitForElement(String locator) {
+		new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
+	}
 
-    public void waitForElement(String locator) {
+	public void assertAndClick(String locator) {
+		assertElementPresentByXpath(locator);
+		driver.findElement(By.xpath(locator)).click();
+	}
 
-        new WebDriverWait(driver, 20)
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
-    }
+	public void assertAndType(String locator, String text) {
+		assertElementPresentByXpath(locator);
+		driver.findElement(By.xpath(locator)).sendKeys(text);
+	}
 
-    public void assertAndClick(String locator) {
+	public void assertElementPresentByXpath(String locator) {
+		logger.info("# Verifying element.");
+		Assert.assertTrue(isElementPresent(locator), "Element " + locator + " not found.");
+	}
 
-        assertElementPresentByXpath(locator);
-        driver.findElement(By.xpath(locator)).click();
+	public void assertElementNotPresentByXpath(String locator) {
+		logger.info("# Verifying element.");
+		Assert.assertFalse(isElementPresent(locator), "Element " + locator + " is found.");
+	}
 
-    }
+	public boolean isElementPresent(String locator) {
+		try {
+			driver.findElement(By.xpath(locator));
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 
-    public void assertAndType(String locator, String text) {
+	public boolean isElementVisible(String locator) {
+		try {
+			return driver.findElement(By.xpath(locator)).isDisplayed();
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 
-        assertElementPresentByXpath(locator);
-        driver.findElement(By.xpath(locator)).sendKeys(text);
+	public void assertElementVisible(String locator, boolean isVisible) {
+		logger.info("# Verifying element visibility.");
+		if (isVisible)
+			Assert.assertTrue(isElementVisible(locator), "Element " + locator + " should be visible.");
+		else
+			Assert.assertFalse(isElementVisible(locator), "Element " + locator + " should not be visible.");
+	}
 
-    }
+	public void waitForElementVisible(String locator) {
+		new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+	}
 
+	public void waitForElementInVisible(String locator) {
+		new WebDriverWait(driver, 20).until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(locator)));
+	}
 
-    public void assertElementPresentByXpath(String locator) {
-        System.out.println("# Verifying element.");
-        Assert.assertTrue(isElementPresent(locator), "Element " + locator + " not found.");
-    }
+	public WebElement getWebElement(String xpath) {
+		return driver.findElement(By.xpath(xpath));
 
-    public void assertElementNotPresentByXpath(String locator) {
-        System.out.println("# Verifying element.");
-        Assert.assertFalse(isElementPresent(locator), "Element " + locator + " is found.");
-    }
-
-    public boolean isElementPresent(String locator) {
-        try {
-            driver.findElement(By.xpath(locator));
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-
-    }
-
-
-    public boolean isElementVisible(String locator) {
-
-        try {
-            return driver.findElement(By.xpath(locator)).isDisplayed();
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-
-    }
-
-    public void assertElementVisible(String locator, boolean isVisible) {
-        System.out.println("# Verifying element visibility.");
-
-        if (isVisible)
-            Assert.assertTrue(isElementVisible(locator), "Element " + locator + " should be visible.");
-        else
-            Assert.assertFalse(isElementVisible(locator),
-                    "Element " + locator + " should not be visible.");
-    }
-
-    public void waitForElementVisible(String locator) {
-
-        new WebDriverWait(driver, 20)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
-    }
-
-    public void waitForElementInVisible(String locator) {
-
-        new WebDriverWait(driver, 20)
-                .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(locator)));
-    }
-
-    public WebElement getWebElement(String xpath) {
-
-        return driver.findElement(By.xpath(xpath));
-
-    }
-
+	}
 
 }
