36841bb171a9482f706d821d9f6aa6665915fe99
Anuj Kumar <cdac.anuj@gmail.com>
2019-04-17 19:11:31 +0530

    Added logger.

--- src/test/java/auto/utility/Init.java
+++ src/test/java/auto/utility/Init.java
@@ -5,53 +5,57 @@ package auto.utility;
  * Date: 12-May-18
  */
 
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
-
 public class Init {
-    protected WebDriver driver = null;
 
-    /**
-     * This function is used for doing web driver setup.
-     */
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() {
-        System.out.println("*** Setup ***");
-        FirefoxProfile ffprofile = createFirefoxProfile();
-        driver = new FirefoxDriver(ffprofile);
-        driver.get("http://the-internet.herokuapp.com/");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-    }
+	static Logger logger = Logger.getLogger(Init.class.getName());
 
-    /**
-     * This function is quit the driver instance.
-     */
-    @AfterMethod(alwaysRun = true)
-    public void teardown() {
-        System.out.println("*** Teardown ***");
-        if (driver != null)
-            driver.quit();
+	protected WebDriver driver = null;
 
-    }
+	/**
+	 * This function is used for doing web driver setup.
+	 */
+	@BeforeMethod(alwaysRun = true)
+	public void setUp() {
+		logger.info("# Setup.");
+		FirefoxProfile ffprofile = createFirefoxProfile();
+		driver = new FirefoxDriver(ffprofile);
+		driver.get("http://the-internet.herokuapp.com/");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
 
-    public FirefoxProfile createFirefoxProfile() {
+	/**
+	 * This function is quit the driver instance.
+	 */
+	@AfterMethod(alwaysRun = true)
+	public void teardown() {
+		logger.info("# Teardown.");
+		if (driver != null)
+			driver.quit();
 
-        FirefoxProfile firefoxProfile = new FirefoxProfile();
+	}
 
-        firefoxProfile = new FirefoxProfile();
-        firefoxProfile.setPreference("browser.download.folderList", 2);
-        firefoxProfile.setPreference("browser.download.dir", "E:\\git_projects\\download");
-        firefoxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk",
-                "text/csv,application/pdf,application/vnd.ms-excel,application/octet-stream");
-        firefoxProfile.setPreference("pdfjs.disabled", true);
+	public FirefoxProfile createFirefoxProfile() {
+		logger.info("# Setting up Firefox profile.");
+		FirefoxProfile firefoxProfile = new FirefoxProfile();
 
-        return firefoxProfile;
-    }
+		firefoxProfile = new FirefoxProfile();
+		firefoxProfile.setPreference("browser.download.folderList", 2);
+		firefoxProfile.setPreference("browser.download.dir", "E:\\git_projects\\download");
+		firefoxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk",
+				"text/csv,application/pdf,application/vnd.ms-excel,application/octet-stream");
+		firefoxProfile.setPreference("pdfjs.disabled", true);
+
+		return firefoxProfile;
+	}
 
 }
