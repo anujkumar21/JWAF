2f5d40fc8598b8b179037ecf30dc8c2a5ca8b9a2
Anuj Kumar <cdac.anuj@gmail.com>
2019-10-29 17:17:40 +0530

    updating POM
    Signed-off-by: Anuj Kumar <cdac.anuj@gmail.com>

--- src/test/java/auto/utility/DriverFactory.java
+++ src/test/java/auto/utility/DriverFactory.java
@@ -13,15 +13,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import static org.apache.log4j.Logger.getLogger;
+
 public class DriverFactory {
-    static Logger logger = Logger.getLogger(DriverFactory.class.getName());
-    private String browser;
+    private static Logger logger = getLogger(DriverFactory.class.getName());
 
     public WebDriver getDriver() {
-
-        browser = System.getProperty("browser");
+        String browser = System.getProperty("browser");
         if (browser == null)
-            browser = "firefox";
+            browser = "chrome";
         logger.info("# WebDriver instance for browser: " + browser);
 
         if (browser.equalsIgnoreCase("chrome"))
@@ -29,25 +29,19 @@ public class DriverFactory {
         else if (browser.equalsIgnoreCase("ie"))
             return new InternetExplorerDriver();
         else {
-            FirefoxProfile ffprofile = createFirefoxProfile();
-            return new FirefoxDriver(ffprofile);
+            //FirefoxProfile ffprofile = createFirefoxProfile();
+            return new FirefoxDriver();
         }
-
     }
 
-    public FirefoxProfile createFirefoxProfile() {
-
+    private FirefoxProfile createFirefoxProfile() {
         logger.info("# Setting up Firefox profile.");
         FirefoxProfile firefoxProfile = new FirefoxProfile();
-
-        firefoxProfile = new FirefoxProfile();
         firefoxProfile.setPreference("browser.download.folderList", 2);
         firefoxProfile.setPreference("browser.download.dir", "E:\\git_projects\\download");
         firefoxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk",
                 "text/csv,application/pdf,application/vnd.ms-excel,application/octet-stream");
         firefoxProfile.setPreference("pdfjs.disabled", true);
-
         return firefoxProfile;
     }
-
 }
